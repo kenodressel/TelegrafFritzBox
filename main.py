89fd4c597f251abf48200f20ed509e7d41f6ec81
kenodressel <hire@keno.digital>
2021-01-16 19:27:01 +0100

    telegraf now reads docker logs

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from telegraf.client import TelegrafClient
+import os
+from subprocess import call
+
+import time
 
 def main():
     setting_keys = [
         "FRITZ_ADDRESS",
         "FRITZ_USERNAME",
         "FRITZ_PASSWORD",
-        "TELEGRAF_HOSTNAME",
-        "TELEGRAF_PORT",
         "SAMPLE_PERIOD"
     ]
     # Check if all environment keys are suplied and if they aren't end the program via an exception
@@ -14,7 +15,7 @@ def main():
     if len(missing_keys) > 0:
         raise Exception(f"You need to supply the environment variable(s): {', '.join(missing_keys)}")
     # Extract the settings into a dictionary
-    settings: Dict[str, Any] = {key: os.environ[key] for key in setting_keys}
+    settings = {key: os.environ[key] for key in setting_keys}
     
     # Print information about the current configuration
     print("Current configuration:")
@@ -23,24 +24,17 @@ def main():
         censored_value = '*'*len(value) if "PASSWORD" in key else value
         print(f"\t{key}={censored_value}")
     print()
-    
-    # Create the telgraf client, this pip library doesn't really do much
-    telegraf_client = TelegrafClient(
-        host=settings["TELEGRAF_HOSTNAME"],
-        port=int(settings["TELEGRAF_PORT"]))
-    
+
     # Set the sample period variable
     SAMPLE_PERIOD = float(settings["SAMPLE_PERIOD"])
     print()
     print(f"Starting to poll metrics every {SAMPLE_PERIOD} seconds")
-    
-    while True:
-        # Send the collected data to telegraf
-        telegraf_client.metric("router", data)
 
-        # Print the data depending on the settings
-        if settings["PRINT_DATA"]:
-            print(data)
+    start = time.time()
+    while True:
+        call("python" + " telegrafFritzBox.py ", shell=True)
+        call("python" + " telegrafFritzSmartHome.py ", shell=True)
+
     
         # Sleep the appropriate amount of time
         time.sleep(SAMPLE_PERIOD - (time.time()-start)%SAMPLE_PERIOD)
